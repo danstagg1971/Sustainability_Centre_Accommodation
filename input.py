7063553c91f53724e827ea9abd07c26013bf625e
danstagg <danstagg@gmail.com>
2023-10-15 11:32:26 +0100

    Added lodge_type variable and added it to the output

--- input.py
+++ input.py
@@ -1,5 +1,33 @@
 # Input section that takes all the customer details
-from global_variables import *
+
+# Constructing the personal variables
+name = "0"
+surname = "0"
+cust_email = "0"
+cust_phone = 0
+number_of_nights = 0
+number_of_people = 0
+accommodation_type = 0
+camping_type = 0
+lodge_type = 0
+confirm_lodge = "0"
+confirm_camping = "0"
+
+# ROOM TYPES
+double_room = ["Double room", 3, 7]
+triple_room = ["Triple room", 1, 2, 4, 8, 9, 10, 11, 12, 13, 14, 15]
+family_room = ["Family room", 4, 5]
+
+# CAMPING PITCHES
+standard_pitch = ["Standard pitch", 1, 2, 3, 4, 5, 6, 7, 8]
+secluded_pitch = ["Secluded pitch", "Forget Me Knot", "Bramble", "Oak"]
+
+# YURTS
+yurts = "Yurt"
+yurt1 = ["Rose"]
+yurt2 = ["Olive"]
+yurt3 = ["Mini"]
+
 
 # Get customer personal details
 def get_cust_info():
@@ -122,9 +150,6 @@ def lodge():
                 break
         except ValueError:
             print("\nPlease try again!")
-
-
-
 
 
 get_cust_info()
